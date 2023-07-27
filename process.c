46932829096545316184952e4c3876e67c8e65e8
Mohammed Mubarak <modymu9@gmail.com>
2023-07-27 14:15:03 +0200

    multilines commands

--- process.c
+++ process.c
@@ -14,7 +14,12 @@ void inner_process(char *str, char **path, char *pname,
 	int found;
 	char buf[MAX_LENGTH];
 	char **args;
+	int i, space = 0;
 
+	for (i = 0; str[i] == ' '; i++)
+		space++;
+	if (space == _strlen(str))
+		return;
 	args = _tokenize(str, " \t\r\n\0");
 	found = check_cmd(args[0], path, buf);
 	switch (found)
@@ -22,32 +27,17 @@ void inner_process(char *str, char **path, char *pname,
 		case 1:
 			/* call eval function */
 			eval(buf, args);
-			if (dynamic->ptr1)
-				free_buf(dynamic->ptr1);
-			if (dynamic->ptr2)
-				free_buf(dynamic->ptr2);
-			free(dynamic);
 			if (args)
 				free_buf(args);
 			break;
 		case 2:
 			built(args, dynamic);
-			if (dynamic->ptr1)
-				free_buf(dynamic->ptr1);
-			if (dynamic->ptr2)
-				free_buf(dynamic->ptr2);
-			free(dynamic);
 			if (args)
 				free_buf(args);
 			break;
 		default:
 			cmd_not_found(pname, command_num, args[0]);
 			fflush(stderr);
-			if (dynamic->ptr1)
-				free_buf(dynamic->ptr1);
-			if (dynamic->ptr2)
-				free_buf(dynamic->ptr2);
-			free(dynamic);
 			if (args)
 				free_buf(args);
 			break;
@@ -65,25 +55,36 @@ void process(char **path, char *pname, int isaty)
 	char *str = NULL, **args_all, **args_semi;
 	int i, semi_len;
 	int command_num = 0;
-	ref_t *dynamic_ref = malloc(sizeof(ref_t));
+	ref_t *dynamic = malloc(sizeof(ref_t));
 
-	dynamic_ref->ptr3 = path;
-	str = _readline(&command_num, isaty, dynamic_ref);
-	args_all = _tokenize(str, "#");
+	dynamic->ptr3 = path;
+	str = _readline(&command_num, isaty, dynamic);
+	args_all = _tokenize(str, "\n");
 	args_semi = _tokenize(args_all[0], ";");
-	dynamic_ref->ptr1 = args_all;
-	dynamic_ref->ptr2 = args_semi;
+	dynamic->ptr1 = args_all;
+	dynamic->ptr2 = args_semi;
 	free(str);
 	for (semi_len = 0; args_semi[semi_len]; semi_len++)
 		continue;
 	if (semi_len > 1)
 	{
 		for (i = 0; args_semi[i]; i++)
-			inner_process(args_semi[i], path, pname, command_num, dynamic_ref);
+			inner_process(args_semi[i], path, pname, command_num, dynamic);
+		if (dynamic->ptr1)
+			free_buf(dynamic->ptr1);
+		if (dynamic->ptr2)
+			free_buf(dynamic->ptr2);
+		free(dynamic);
 	}
 	else
 	{
-		inner_process(args_all[0], path, pname, command_num, dynamic_ref);
+		for (i = 0; args_all[i]; i++)
+			inner_process(args_all[i], path, pname, command_num, dynamic);
+		if (dynamic->ptr1)
+			free_buf(dynamic->ptr1);
+		if (dynamic->ptr2)
+			free_buf(dynamic->ptr2);
+		free(dynamic);
 	}
 }
 
